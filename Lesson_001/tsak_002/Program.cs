246dac6cefd189776a707ab401eae1ade2534551
Zver-gus <keeper95555@gmail.com>
2022-12-25 23:58:34 +0400

    create task_002

--- Lesson_001/tsak_002/Program.cs
+++ Lesson_001/tsak_002/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if (num1 > num2)
+{
+    if (num1 > num3)
+    {
+        max = num1;
+    }
+    else
+    {
+        max = num3;
+    }
+}
+else if (num2 > num3)
+{
+    max = num2;
+}
+    else 
+    {
+        max = num3;
+    }
+
+Console.Write("Наибольшее число = ");
+Console.WriteLine(max);
