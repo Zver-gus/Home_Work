ff62ebb62c325b7c853086df07682760b8a3095c
Zver-gus <keeper95555@gmail.com>
2022-12-26 23:56:37 +0400

    create task_004

--- Lesson_001/task_004/Program.cs
+++ Lesson_001/task_004/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число n ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (i <= n)
+{
+    if (i % 2 == 0)
+    {
+         Console.Write(i);
+         Console.Write(", ");
+    }
+    i++;
+}
