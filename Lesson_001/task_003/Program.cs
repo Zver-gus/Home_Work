2d51f118bbc5f404aff807eb45fe772c2e2cd1c4
Zver-gus <keeper95555@gmail.com>
2022-12-26 23:42:31 +0400

    create task_003

--- Lesson_001/task_003/Program.cs
+++ Lesson_001/task_003/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число для проверки на чётность ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 % 2 == 0)
+{
+    Console.WriteLine("Чётное");
+}
+else
+{
+    Console.WriteLine("Не чётное");
+}
